3959f8b0d841e8823e21e428bf79437bc7f105f7
Michael Kokonowskyj <mikokono@microsoft.com>
2022-09-15 11:55:23 +0200

    fixed tf errors

--- eng/tf/vars.tf
+++ eng/tf/vars.tf
@@ -22,22 +22,3 @@ variable "sql_admin_password" {
     type = string
     default = "4-v3ry-53cr37-p455w0rd"
 }
-
- variable "resource_group_name"{
-    type = string
-    default =  "rg-githubactions-tf-states"
- }
-  
-  varialbe   "storage_account_name"{
-    type = string
-    default ="mkgithubtfstates"
-  }
-  variable  "container_name"{
-    type = string
-    default ="tfstatedevops"
-  } 
-
-  variable "key" {
-    type = string
-    default ="terraformgithubexample.tfstate"
-  }
