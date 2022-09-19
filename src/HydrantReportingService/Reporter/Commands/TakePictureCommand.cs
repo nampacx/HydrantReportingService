32f6d462d04f5d7cdcd4317dff0d6e90395b9a9d
Michael Kokonowskyj <mikokono@microsoft.com>
2022-09-19 12:19:06 +0200

    added some reporting commands

---
+++ src/HydrantReportingService/Reporter/Commands/TakePictureCommand.cs
@@ -0,0 +1,52 @@
+﻿using Reporter.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Reporter.Commands
+{
+    internal class TakePictureCommand : ICommand
+    {
+        private readonly ReporterViewModel reporterViewModel;
+
+        public event EventHandler CanExecuteChanged;
+
+        public TakePictureCommand(ReporterViewModel reporterViewModel)
+        {
+            this.reporterViewModel = reporterViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;        
+        }
+
+        public void Execute(object parameter)
+        {
+            TakePhoto();
+        }
+
+        private async void TakePhoto()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {
+                    // save the file into local storage
+                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                    using Stream sourceStream = await photo.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                    await sourceStream.CopyToAsync(localFileStream);
+                    reporterViewModel.ImagePaths.Add(localFilePath);
+                }
+            }
+        }
+    }
+}
---
+++ src/HydrantReportingService/Reporter/ViewModel/ReporterViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Reporter.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Reporter.ViewModel
+{
+    public class ReporterViewModel : INotifyPropertyChanged
+    {
+        private Location location;
+        private string locationString;
+        private ObservableCollection<string> imagePaths;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public Location Location { get => location; set => SetProperty(ref location, value); }
+        public string LocationString { get => locationString; set => SetProperty(ref locationString, value); }
+
+        public ObservableCollection<string> ImagePaths { get => imagePaths; set => SetProperty(ref imagePaths, value); }
+
+        public ICommand AddPictureCommand { get; private set; }
+        public ICommand GetLocationCommand { get; private set; }
+        public ICommand TakePictureCommand { get; private set; } 
+
+        public ICommand RemovePictureCommand { get; private set; }
+        public ReporterViewModel()
+        {
+            RemovePictureCommand = new RemovePictureCommand(this);
+            AddPictureCommand = new AddPicture(this);
+            GetLocationCommand = new GetLocationCommand(this);
+            TakePictureCommand = new TakePictureCommand(this);
+            ImagePaths = new ObservableCollection<string>();
+        }
+    }
+}
