9eeaf33d5c420a306a82ada07c9267b7a585a671
Michael Kokonowskyj <mikokono@microsoft.com>
2022-09-20 15:36:12 +0200

    ranme

--- eng/tf/main.tf
+++ eng/tf/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_service_plan" "svc_plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "B1"
 }
 
 resource "azurerm_application_insights" "ai" {
