e4684508b430419356688e2656664dbd2f5d7cf4
Sven Pohl <sven.pohl@microsoft.com>
2022-09-21 08:46:48 +0200

    add map component

--- src/HydrantReportingService/Web/projects/hydrant-reporting-service-web/src/app/app.component.ts
+++ src/HydrantReportingService/Web/projects/hydrant-reporting-service-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HydrantReportingServiceWeb';
+
+  private map!: Map;
+  private zoom!: number;
+
+  receiveMap(map: Map) {
+    this.map = map;
+  }
+
+  receiveZoom(zoom: number) {
+    this.zoom = zoom;
+  }
+
 }
